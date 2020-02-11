d64ff8049995d7842b6d8cf17e71701bd4ffce74
zdog59 <rogdog2006@yahoo.com>
2020-02-10 21:36:25 -0500

    Show all snapshotsin list if --all is given

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -28,8 +28,10 @@ def snapshots():
 #@click.command()
 @click.option('--project', default=None,
     help="Only snapshots for project (tag Project:<name>)")
+@click.option('--all', 'list_all', default=False, is_flag=True,
+    help="Liost all snapshots for each vloume, not just the most recent.")
 
-def list_snapshots(project):
+def list_snapshots(project, list_all):
     "List volume snapshots"
     instances = filter_instances(project) 
 
@@ -44,6 +46,8 @@ def list_snapshots(project):
                 s.progress,
                 s.start_time.strftime("%c")
             )))
+
+            if s.state == 'completed'and not list_all: break
     return      
 
 @cli.group('volumes')
